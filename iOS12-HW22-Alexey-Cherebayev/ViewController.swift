c4ad26577889c743cb7f0a14b53d0de9af8f7583
AlexChe360 <devalex360@gmail.com>
2024-04-22 07:33:43 +0500

    Initial Commit

---
+++ iOS12-HW22-Alexey-Cherebayev/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS12-HW22-Alexey-Cherebayev
+//
+//  Created by Alex on 22.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
